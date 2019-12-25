aaa6d2131535abbbdbac5e87caf601ccedc033c4
auu788 <auu788@users.noreply.github.com>
2019-12-25 13:48:29 +0100

    Deploy

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ app.post('/', (req, res) => {
         case 'MESSAGE':
             const response = res.json(renderResultsCard('Wyniki', playerScores));
 
-            console.log('PAZNA RES: ' + JSON.stringify(response));
+            console.log('PAZNA RES: ' + JSON.stringify(renderResultsCard('Wyniki', playerScores)));
             return response;
         case 'CARD_CLICKED':
             return res.json({text: `You wanted to make an action ${action.actionMethodName} with parameters: ${action.parameters}`});
