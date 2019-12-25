ee3091dac525c282bf637ec331c2c75d0755b134
auu788 <auu788@users.noreply.github.com>
2019-12-25 14:20:37 +0100

    Deploy

--- src/app.js
+++ src/app.js
@@ -1,30 +1,8 @@
 const express = require('express');
 const SpaceService = require('./services/SpaceService');
+const MessageService = require('./services/MessageService');
 
 const PORT = process.env.PORT || 8888;
-
-const playerScores = [
-    {
-        'place': 1,
-        'alias': 'Radek',
-        'shoots': 7
-    },
-    {
-        'place': 2,
-        'alias': 'Fifonż',
-        'shoots': 5
-    },
-    {
-        'place': 3,
-        'alias': 'Tomek',
-        'shoots': 4
-    },
-    {
-        'place': 4,
-        'alias': 'Kuba',
-        'shoots': 1
-    }
-];
 
 const app = express()
     .use(express.urlencoded({extended: false}))
@@ -32,16 +10,13 @@ const app = express()
 
 app.post('/', (req, res) => {
     console.log('PAZNA: ' + JSON.stringify(req.body));
-    const { space, user, type, message, action } = req.body || {};
+    const { type, message, action } = req.body || {};
 
     switch (type) {
         case 'ADDED_TO_SPACE':
             return res.json(SpaceService.response(req.body));
         case 'MESSAGE':
-            const response = res.json(renderResultsCard('Wyniki', playerScores));
-
-            console.log('PAZNA RES: ' + JSON.stringify(renderResultsCard('Wyniki', playerScores)));
-            return response;
+            return res.json(MessageService.response(message));
         case 'CARD_CLICKED':
             return res.json({text: `You wanted to make an action ${action.actionMethodName} with parameters: ${action.parameters}`});
         default:
@@ -52,108 +27,3 @@ app.post('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running in port - ${PORT}`);
 });
-
-const renderResultsCard = (title, playerScores) => {
-    let response = {};
-    response.cards = [];
-
-    const card = {};
-    card.header = renderCardHeader(title);
-    card.sections = [
-        renderResultsSection(playerScores),
-        renderButtonsSection()
-    ];
-    response.cards.push(card);
-
-    return response;
-};
-
-const renderResultsSection = (playerScores) => {
-    let resultsSection = {};
-    resultsSection.widgets = playerScores.map(renderResultsPlace);
-
-    return resultsSection;
-};
-
-const renderButtonsSection = () => {
-    let buttonsSection = {};
-    buttonsSection.widgets = [];
-
-    let buttons = [];
-    buttons.push(renderTextButton('Dzienne', 'getResults', 'daily'));
-    buttons.push(renderTextButton('Tygodniowe', 'getResults', 'weekly'));
-    buttons.push(renderTextButton('Miesięczne', 'getResults', 'monthly'));
-    buttonsSection.widgets.push({
-        'buttons': buttons
-    });
-
-    return buttonsSection;
-};
-
-const renderTextButton = (buttonName, actionMethodName, actionType) => {
-    let button = {};
-    button.textButton = {};
-    button.textButton.text = buttonName;
-    button.textButton.onClick = {};
-    button.textButton.onClick.action = {};
-    button.textButton.onClick.action.actionMethodName = actionMethodName;
-    button.textButton.onClick.action.parameters = [
-        {
-            "key": "period",
-            "value": actionType
-        }
-    ];
-
-    return button;
-};
-
-const renderCardHeader = (title) => {
-    return {title};
-};
-
-const renderResultsPlace = (playerScoreData) => {
-    const { alias, place, shoots } = playerScoreData;
-
-    return {
-        'keyValue': {
-            'iconUrl': getPlaceIconUrl(place),
-            'topLabel': generateTopLabel(place),
-            'content': alias,
-            'bottomLabel': generateBottomLabel(shoots)
-        }
-    }
-};
-
-const getPlaceIconUrl = (place) => {
-    let url = 'https://ssl.gstatic.com/dynamite/emoji/png/128/';
-
-    switch (place) {
-        case 1:
-            return url + 'emoji_u1f947.png';
-        case 2:
-            return url + 'emoji_u1f948.png';
-        case 3:
-            return url + 'emoji_u1f949.png';
-        default:
-            return url + 'emoji_u1f636.png';
-    }
-};
-
-const generateTopLabel = (place) => {
-    let text = `${place} miejsce`;
-    if (place === 1) {
-        text += ' - Zwycięzca';
-    }
-
-    return text;
-};
-
-const generateBottomLabel = (shootsNum) => {
-    if (shootsNum === 1) {
-        return `${shootsNum} rzut trafiony`;
-    } else if (2 <= shootsNum && shootsNum <= 4) {
-        return `${shootsNum} rzuty trafione`;
-    } else {
-        return `${shootsNum} rzutów trafionych`;
-    }
-};
---
+++ src/services/MessageService.js
@@ -0,0 +1,132 @@
+const playerScores = [
+    {
+        'place': 1,
+        'alias': 'Radek',
+        'shoots': 7
+    },
+    {
+        'place': 2,
+        'alias': 'Fifonż',
+        'shoots': 5
+    },
+    {
+        'place': 3,
+        'alias': 'Tomek',
+        'shoots': 4
+    },
+    {
+        'place': 4,
+        'alias': 'Kuba',
+        'shoots': 1
+    }
+];
+
+exports.response = (messageBody) => {
+    const { sender, argumentText } = messageBody || {};
+    return renderResultsCard('Wyniki', playerScores);
+};
+
+const renderResultsCard = (title, playerScores) => {
+    let response = {};
+    response.cards = [];
+
+    const card = {};
+    card.header = renderCardHeader(title);
+    card.sections = [
+        renderResultsSection(playerScores),
+        renderButtonsSection()
+    ];
+    response.cards.push(card);
+
+    return response;
+};
+
+const renderResultsSection = (playerScores) => {
+    let resultsSection = {};
+    resultsSection.widgets = playerScores.map(renderResultsPlace);
+
+    return resultsSection;
+};
+
+const renderButtonsSection = () => {
+    let buttonsSection = {};
+    buttonsSection.widgets = [];
+
+    let buttons = [];
+    buttons.push(renderTextButton('Dzienne', 'getResults', 'daily'));
+    buttons.push(renderTextButton('Tygodniowe', 'getResults', 'weekly'));
+    buttons.push(renderTextButton('Miesięczne', 'getResults', 'monthly'));
+    buttonsSection.widgets.push({
+        'buttons': buttons
+    });
+
+    return buttonsSection;
+};
+
+const renderTextButton = (buttonName, actionMethodName, actionType) => {
+    let button = {};
+    button.textButton = {};
+    button.textButton.text = buttonName;
+    button.textButton.onClick = {};
+    button.textButton.onClick.action = {};
+    button.textButton.onClick.action.actionMethodName = actionMethodName;
+    button.textButton.onClick.action.parameters = [
+        {
+            "key": "period",
+            "value": actionType
+        }
+    ];
+
+    return button;
+};
+
+const renderCardHeader = (title) => {
+    return {title};
+};
+
+const renderResultsPlace = (playerScoreData) => {
+    const { alias, place, shoots } = playerScoreData;
+
+    return {
+        'keyValue': {
+            'iconUrl': getPlaceIconUrl(place),
+            'topLabel': generateTopLabel(place),
+            'content': alias,
+            'bottomLabel': generateBottomLabel(shoots)
+        }
+    }
+};
+
+const getPlaceIconUrl = (place) => {
+    let url = 'https://ssl.gstatic.com/dynamite/emoji/png/128/';
+
+    switch (place) {
+        case 1:
+            return url + 'emoji_u1f947.png';
+        case 2:
+            return url + 'emoji_u1f948.png';
+        case 3:
+            return url + 'emoji_u1f949.png';
+        default:
+            return url + 'emoji_u1f636.png';
+    }
+};
+
+const generateTopLabel = (place) => {
+    let text = `${place} miejsce`;
+    if (place === 1) {
+        text += ' - Zwycięzca';
+    }
+
+    return text;
+};
+
+const generateBottomLabel = (shootsNum) => {
+    if (shootsNum === 1) {
+        return `${shootsNum} rzut trafiony`;
+    } else if (2 <= shootsNum && shootsNum <= 4) {
+        return `${shootsNum} rzuty trafione`;
+    } else {
+        return `${shootsNum} rzutów trafionych`;
+    }
+};
