2a944b21117f1feac34fce6eb9f9caff9c0870b2
auu788 <auu788@users.noreply.github.com>
2019-12-26 17:44:51 +0100

    Deploy

--- src/db.js
+++ src/db.js
@@ -2,11 +2,7 @@ const { Pool } = require('pg');
 const ValidationError = require('./ValidationError');
 
 const db = new Pool({
-    user: 'pdsyxtszaefztw',
-    host: 'ec2-54-217-225-16.eu-west-1.compute.amazonaws.com',
-    database: 'dfn2234ll8u8cf',
-    password: '1eb35ea4c0c595a04efead2a38adf6c4818dd3f97d69b5e2503a2d1c4e639d34',
-    port: 5432,
+    database: process.env.DATABASE_URL
 });
 
 const createPlayer = async (firstName, lastName, alias) => {
