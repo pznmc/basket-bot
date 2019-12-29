9daf4d89405618d163cd7eb58121a23f401bc503
auu788 <auu788@users.noreply.github.com>
2019-12-29 19:48:15 +0100

    Deploy

--- src/services/MessageService.js
+++ src/services/MessageService.js
@@ -34,7 +34,7 @@ const handleGetResults = async (msgCommand) => {
     let dateWhereClause = '';
 
     if (msgCommand === 'wyniki') {
-        throw new ValidationError('Dostępne komendy: ```wyniki - ostatni dzień\nwyniki - ostatni tydzień\nwyniki - ostatni rok');
+        throw new ValidationError('Dostępne komendy: ```wyniki - ostatni dzień\nwyniki - ostatni tydzień\nwyniki - ostatni rok```');
     } else {
         if (msgCommand.includes('ostatni dzień')) {
             dateWhereClause = '> CURRENT_DATE - 1';
